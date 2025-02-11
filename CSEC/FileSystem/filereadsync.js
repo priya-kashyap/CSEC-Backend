7338710737fbcfb320c23e625c009165d88abb3d
PRIYA KASHYAP <priykashyap1214xii@gmail.com>
2025-02-11 10:13:39 +0530

    changes in file

---
+++ CSEC/FileSystem/filereadsync.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const data = fs.readFileSync("./data.txt","utf-8");
+console.log(data)
+if(data.match("H")){
+    console.log("File contains 'H'");
+    const newdata=data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
+//fs.writeFileSync("./data1.txt","Hello ABES","utf-8");
+//fs.writeFileSync("./data1.txt","Hello ABESEC","utf-8");
+//fs.appendFileSync("./data1.txt","Students","utf-8");
+//fs.renameSync("./data2.txt","./data3.txt");
+//fs.unlinkSync("./data3.txt");
+
+
+
